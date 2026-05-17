cc48a21b91a0fd1acc807f53ea3bd4ce41dcd9b8
Mykola Mokhnach <mokhnach@gmail.com>
2026-05-17 19:15:32 +0200

    add e2e test on real device

---
+++ test/functional/basic/no-app-page-source-e2e-specs.ts
@@ -0,0 +1,38 @@
+import {initSession, deleteSession, MOCHA_TIMEOUT} from '../helpers/session';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+
+const REAL_DEVICE_UDID = '00008030-000A49391460202E';
+// const WDA_BUNDLE_ID = 'com.appium.WebDriverAgentRunner1234';
+
+function getRealDeviceCaps() {
+  return {
+    'appium:udid': REAL_DEVICE_UDID,
+    'appium:platformVersion': '26.3',
+    'appium:automationName': 'XCUITest',
+    'appium:noReset': true,
+    'platformName': 'iOS',
+  };
+}
+
+describe('XCUITestDriver - real device no app page source -', function () {
+  this.timeout(MOCHA_TIMEOUT);
+
+  let driver;
+
+  before(async function () {
+    driver = await initSession(getRealDeviceCaps());
+  });
+
+  after(async function () {
+    await deleteSession();
+  });
+
+  it('should start a session without an app and print page source', async function () {
+    const pageSource = await driver.getPageSource();
+    // eslint-disable-next-line no-console
+    console.log(pageSource);
+  });
+});
